3a21920edcf20a1e5807fcc0b189d08bf2f1273d
thisisaaronland <straup@gmail.com>
2018-06-01 16:30:19 -0700

    make it work with pip; bump to 0.4.0

--- setup.py
+++ setup.py
@@ -1,19 +1,38 @@
 #!/usr/bin/env python
 
+import os
+import sys
+
+setup = os.path.abspath(sys.argv[0])
+parent = os.path.dirname(setup)
+pkg = os.path.basename(parent)
+
+if pkg.startswith("py-cooperhewitt"):
+    pkg = pkg.replace("py-", "")
+    pkg = pkg.replace("-", ".")
+
+    egg_info = "%s.egg-info" % pkg
+    egg_info = os.path.join(parent, egg_info)
+
+    if os.path.exists(egg_info):
+        shutil.rmtree(egg_info)
+
 from setuptools import setup, find_packages
 
 packages = find_packages()
 desc = open("README.md").read()
+version = open("VERSION").read()
 
-setup(name='cooperhewitt-swatchbook',
-      version='0.3',
-      description='Cooper Hewitt\'s Python tools for wrangling colours',
-      long_description=desc,
-      author='Cooper Hewitt Smithsonian Design Museum',
-      url='https://github.com/cooperhewitt/py-cooperhewitt-swatchbook',
-      requires=['colorsys', 'webcolors', 'colormath (<2.0)'],
-      install_requires=['webcolors', 'colormath<2.0'],
-      packages=packages,
-      scripts=[],
-      download_url='https://github.com/cooperhewitt/py-cooperhewitt-swatchbook/releases/tag/v0.3',
-      license='BSD')
+setup(
+    name='cooperhewitt-swatchbook',
+    namespace_packages=['cooperhewitt'],
+    version=version,
+    description='Cooper Hewitt\'s Python tools for wrangling colours',
+    long_description=desc,
+    author='Cooper Hewitt Smithsonian Design Museum',
+    url='https://github.com/cooperhewitt/py-cooperhewitt-swatchbook',
+    packages=packages,
+    scripts=[],
+    download_url='https://github.com/cooperhewitt/py-cooperhewitt-swatchbook/releases/tag/' + version,
+    license='BSD'
+)
